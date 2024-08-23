63714b382ba1669cf997e4814182aaf573037525
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-23 15:50:54 +0200

    Creating token

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,29 +1,12 @@
+require 'livekit'
 class RoomsController < ApplicationController
-    def create
-      room_name = params[:room_name]
-      livekit_service = LiveKitService.new(ENV['LIVEKIT_API_KEY'], ENV['LIVEKIT_API_SECRET'], ENV['LIVEKIT_SERVER_URL'])
-      @room = livekit_service.create_room(room_name)
-      render json: @room
-    end
 
-    def generate_token(api_key, api_secret, room_name, participant_identity, ttl = 600)
-        payload = {
-          "iss" => api_key,
-          "sub" => participant_identity,
-          "room" => room_name,
-          "exp" => Time.now.to_i + ttl,
-          "video" => true,
-          "audio" => true
-        }
-        JWT.encode(payload, api_secret, 'HS256')
-      end
-  
     def show
-      room_name = params[:room_name]
-      participant_identity = params[:participant_identity]
-      livekit_service = LiveKitService.new(ENV['LIVEKIT_API_KEY'], ENV['LIVEKIT_API_SECRET'], ENV['LIVEKIT_SERVER_URL'])
-      @token = livekit_service.join_room(room_name, participant_identity)
-      render json: @token
+      token = LiveKit::AccessToken.new(api_key: 'devkey', api_secret: 'secret')
+      token.identity = params[:participant_identity]
+      token.add_grant(roomJoin: true, room: params[:room_name])
+
+      render json: {token: token.to_jwt, identity: params[:participant_identity], roomName: params[:room_name]}
     end
   end
   
