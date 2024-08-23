4319480e2b5158855ced921f3166a1bad2c0219c
fogadOg <fogadm06@gmail.com>
2024-08-23 15:41:21 +0200

    moving generate token

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,8 +5,20 @@ class RoomsController < ApplicationController
       @room = livekit_service.create_room(room_name)
       render json: @room
     end
+
+    def generate_token(api_key, api_secret, room_name, participant_identity, ttl = 600)
+        payload = {
+          "iss" => api_key,
+          "sub" => participant_identity,
+          "room" => room_name,
+          "exp" => Time.now.to_i + ttl,
+          "video" => true,
+          "audio" => true
+        }
+        JWT.encode(payload, api_secret, 'HS256')
+      end
   
-    def join
+    def show
       room_name = params[:room_name]
       participant_identity = params[:participant_identity]
       livekit_service = LiveKitService.new(ENV['LIVEKIT_API_KEY'], ENV['LIVEKIT_API_SECRET'], ENV['LIVEKIT_SERVER_URL'])
