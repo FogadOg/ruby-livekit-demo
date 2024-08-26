6783d2fe3f141fb0af569178fcd60bb523e9b50f
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-26 09:01:30 +0200

    Able to use javascript sdk

--- app/javascript/controllers/room_controller.js
+++ app/javascript/controllers/room_controller.js
@@ -1,7 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
+
+
 export default class extends Controller {
+  static values = { token: String }
+
   connect() {
-    this.element.textContent = "Hello World!"
+    const room = new LivekitClient.Room({
+      // automatically manage subscribed video quality
+      adaptiveStream: true,
+
+      // optimize publishing bandwidth and CPU for published tracks
+      dynacast: true,
+    });
+
+    console.log(room)
+    // pre-warm connection, this can be called as early as your page is loaded
+    room.prepareConnection("ws://localhost:7880", this.tokenValue);
+
   }
 }
