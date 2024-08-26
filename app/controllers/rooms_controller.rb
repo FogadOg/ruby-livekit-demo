d3084e965f97bbcb853505b2ab74ebf1170c1ab0
FarouqJalabi <farouqjalabi@gmail.com>
2024-08-26 10:09:10 +0200

    id and room_name required with error if not

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,14 @@
 require 'livekit'
 class RoomsController < ApplicationController
-
     def show
+
+      if params[:id].blank? || params[:room_name].blank?
+        render json: { error: 'id and room_name are required' }, status: 402
+        return
+      end
+
       token = LiveKit::AccessToken.new(api_key: 'devkey', api_secret: 'secret')
-      token.identity = params[:participant_identity]
+      token.identity = params[:id]
       token.add_grant(roomJoin: true, room: params[:room_name])
 
       # render json: {token: token.to_jwt, identity: params[:participant_identity], roomName: params[:room_name]}
