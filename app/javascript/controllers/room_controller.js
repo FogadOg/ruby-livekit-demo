0680fc64a8c2bf8450df67bca0de67d1ce66442a
fogadOg <fogadm06@gmail.com>
2024-08-26 10:58:27 +0200

    getting camera id

--- app/javascript/controllers/room_controller.js
+++ app/javascript/controllers/room_controller.js
@@ -1,11 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 
-
 export default class extends Controller {
   static values = { token: String }
 
-  connect() {
+  async getDeviceIds() {
+
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const device = devices[4]
+    console.log("device: ",device);
+    return device["deviceId"]
+  }
+  
+
+  async connect() {
     const room = new LivekitClient.Room({
       // automatically manage subscribed video quality
       adaptiveStream: true,
@@ -16,8 +24,44 @@ export default class extends Controller {
 
     console.log(room)
     // pre-warm connection, this can be called as early as your page is loaded
-    room.prepareConnection("ws://127.0.0.1:7880", this.tokenValue);
-    room.connect("ws://127.0.0.1:7880", this.tokenValue);
+    room.prepareConnection("ws://localhost:7880", this.tokenValue);
+    
+    room.on('trackSubscribed', handleTrackSubscribed);
+    
+    await room.connect('ws://localhost:7880', this.tokenValue);
 
+    const cameraId = getDeviceIds()
+    
+    const participant = room.localParticipant
+
+    
+    await participant.setCameraEnabled(true, {
+      deviceId: cameraId,
+    });
+
+
+
+    // console.log("participant: ",participant);
+    // await participant.setCameraEnabled(true);
+    // await participant.setMicrophoneEnabled(true);
+
+    // await participant.setScreenShareEnabled(true);
+
+    
+
+    function handleTrackSubscribed(
+      track,
+    ) {
+      console.log("track: ",track);
+
+      if (track.kind === Track.Kind.Video || track.kind === Track.Kind.Audio) {
+        const element = track.attach();
+        console.log("element: ",element);
+        
+        parentElement.appendChild(element);
+      }
+    }
+
+    
   }
 }
